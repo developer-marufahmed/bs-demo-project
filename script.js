8423b961e044f417f57b3788305febe03700beed
MD. MARUF AHMED <139913885+developer-marufahmed@users.noreply.github.com>
2023-09-13 00:03:31 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Bootstrap Tooltip Start//
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
+
+// Bootstrap Tooltip Ends//
